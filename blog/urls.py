c6cd1db21c641abbc295d988cfcec9c5dd3a6dd9
viveklele <viveklele123@gmail.com>
2023-05-08 07:28:58 +0530

    carousel added successfully for images

--- blog/urls.py
+++ blog/urls.py
@@ -1,26 +1,14 @@
 from django.urls import path
-# from .views import #PostDeleteView #PostUpdateView #UserPostListView #,PostCreateView PostDetailView, PostListView
 from . import views
 
 urlpatterns = [
-	# path('', PostListView.as_view(), name='blog-home'),
 	path('', views.post_list, name='blog-home'),
-	# path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 	path('user/<str:username>/', views.user_post_list, name='user-posts'),
-	# path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 	path('post/<int:pk>/', views.post_detail_view, name='post-detail'),
-	# path('postform/', PostCreateView.as_view(), name='post-create'),
-	# path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 	path('post/<int:pk>/update/', views.post_update, name='post-update'),
-	# path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 	path('post/<int:pk>/delete/', views.post_delete, name='post-delete'),
 	path('about/', views.about, name='blog-about'),
 	path('grid_view/', views.grid_view, name='blog-grid_view'),
-	# path('Grid_view/', PostGridView.as_view(), name='blog-grid'),
 	path('search/', views.search, name='search'),
-	# path('post_create_title', views.post_create_title, name='create-post')
 	path('post/new/', views.post_create, name='post-create')
-	# path('search/', views.search_address, name='search')  # for suggetions in search bar
-	
-	
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
-# from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
-# from django.views.generic import ListView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Media
 from users.models import Profile
 from django.db.models import Q
 from django.core.paginator import Paginator 
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from os import remove
 
 def post_list(request):
 	all_posts = Post.objects.all().order_by('-date_posted')
@@ -32,9 +31,11 @@ def user_post_list(request, username):
 	return render(request, "blog/user_posts.html", context)
 
 def post_detail_view(request, pk):
-	data = get_object_or_404(Post, pk=pk)
+	posts = get_object_or_404(Post, pk=pk)
+	media = Media.objects.filter(post=posts)
 	context = {
-		"post" : data
+		"post" : posts,
+		"images" : media
 	}
 	
 	return render(request, 'blog/post_detail.html', context)
@@ -43,21 +44,22 @@ def post_detail_view(request, pk):
 def post_create(request):
 	if request.method == 'POST':
 		title = request.POST.get('title')	
-		content = request.POST.get('content')#
-		blog_image = request.FILES['blog_image']	
+		content = request.POST.get('content')
 		audio = request.FILES['audio']
 		video = request.FILES['video']
 		keywords = request.POST.get('keywords')
-		form = Post(title=title, content=content, blog_image=blog_image, audio=audio, video=video, keywords=keywords)
-		form.author = request.user
+		form = Post(title=title, content=content, audio=audio, video=video, keywords=keywords, author=request.user)
 		form.save()
-
+		for image in request.FILES.getlist('blog_image'):
+			media = Media.objects.create(post=form, blog_image=image)
+			media.save()
 		return redirect('blog-home')
 	return render(request, "blog/post_create.html")
 	
 @login_required
 def post_update(request, pk):
 	form = get_object_or_404(Post, pk=pk)
+	media = Media.objects.filter(post=pk)
 	if request.method == 'POST':
 		form.title = request.POST.get('title')
 		form.content = request.POST.get('content')
@@ -68,28 +70,50 @@ def post_update(request, pk):
 		if 'video' in request.FILES:
 			form.video = request.FILES['video']
 
-		if 'blog_image' in request.FILES:
-			form.blog_image = request.FILES['blog_image']
-
 		form.keywords = request.POST.get('keywords')
 		form.author=request.user
 		form.save()
+		# if post.blog_image:
+		# 	storage, path = post.blog_image.storage, post.blog_image.path
+		# 	storage.delete(path)
+
+		# for image in media:
+		# 	if image.blog_image:
+		# 		storage, path = image.blog_image.storage, image.blog_image.path
+		# 		storage.delete(path)
+		# 	image.delete()
+		if 'blog_image' in request.FILES:
+			for image in media:
+				storage, path = image.blog_image.storage, image.blog_image.path
+				storage.delete(path)
+				image.delete()
+			for image in request.FILES.getlist('blog_image'):
+				new_media = Media(post=form, blog_image=image)
+				new_media.save()
+		# if 'blog_image' in request.FILES:
+		# 	form.blog_image = request.FILES['blog_image']
+
 		messages.success(request, "Your Post update sucessfully!")
 		return redirect('/')
 	context = {
 		'form': form,
+		'images': media
 	}
 	return render(request, 'blog/post_update.html', context)
 
 @login_required
 def post_delete(request, pk):
 	post = get_object_or_404(Post, pk=pk)
+	media = Media.objects.filter(post=pk)
 	context= {'post': post}
 	if request.method == 'POST':
 		# To delete media files from server
-		if post.blog_image:
-			storage, path = post.blog_image.storage, post.blog_image.path
-			storage.delete(path)
+		for image in media:
+			if image.blog_image:
+				storage, path = image.blog_image.storage, image.blog_image.path
+				storage.delete(path)
+				image.delete()
+			
 		if post.audio:
 			storage, path = post.audio.storage, post.audio.path
 			storage.delete(path)
@@ -113,8 +137,6 @@ def grid_view(request):
 	}	
 	return render(request, 'blog/gridview.html', context)
 
-# return search query
-
 def search(request):
 	query = request.GET['query']
 
@@ -123,12 +145,10 @@ def search(request):
 		user = Profile.objects.none()
 	else:
 		allPosts = Post.objects.filter(Q(title__icontains=query) | Q(author__username__icontains=query) | 
-		Q(keywords__icontains=query))
+		Q(keywords__icontains=query)).order_by('-date_posted')
 
 	paras = {
 		'allPosts': allPosts, 'query': query
 	}
-	
 	return render(request, 'blog/search.html', paras)
 
-# show suggetions in search bar
